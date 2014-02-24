7e2b287911e4297765ee99a10198be04a3203307
Baxter Campbell <lsmfx1@hotmail.com>
2014-02-23 23:58:08 -0800

    Added saving, player class, and some css

--- AClickWebsite/ArcaneClickScript.js
+++ AClickWebsite/ArcaneClickScript.js
@@ -1,26 +1,53 @@
 ﻿const increaseRate = 1.25;
-var magick = 0;
-var puddles = 0;
+var puddleCost = 10;
+
+var player = {
+totalClicks: 0,
+magick: 0,
+puddles: 0,
+interval_auto: null
+}
+
+function save_game() {
+    localStorage['ArcaneClicker_save'] = btoa(JSON.stringify(player));
+}
+
+function load_game() {
+    if (!localStorage['ArcaneClicker_save']) return;
+    var save_data = JSON.parse(atob(localStorage['ArcaneClicker_save']));
+    player = save_data;
+    update_view();
+}
 
 function magickClick(number) {
-    magick = magick + number;
-    document.getElementById("magick").innerHTML = magick;
+    player.magick += number;
+    player.totalClicks++;
+    update_view();
 };
 
 function puddleClick(number) {
-    var puddleCost = Math.floor(10 * Math.pow(increaseRate, puddles));
-    if (magick >= puddleCost) {
-        puddles++;
-        magick -= puddleCost;
-        document.getElementById('puddles').innerHTML = puddles;
-        document.getElementById("magick").innerHTML = magick;
-        puddleCost = Math.floor(puddleCost * increaseRate);
+    if (player.magick >= puddleCost) {
+        player.puddles++;
+        player.magick -= puddleCost;
+        puddleCost = Math.floor(10 * Math.pow(increaseRate,player.puddles));
     }
-    document.getElementById("puddleCost").innerHTML = puddleCost;
+    update_view();
 };
 
-window.setInterval(function () {
+function update_view() {
+    document.getElementById('puddles').innerHTML = player.puddles;
+    document.getElementById("magick").innerHTML = player.magick;
+    document.getElementById("puddleCost").innerHTML = puddleCost;
+}
 
-    magickClick(puddles);
+function update_time() {
+    clearInterval(player.interval_auto);
+    player.interval_auto = setInterval(function () {
+        magickClick(player.puddles);
+    }, 1000);
+}
 
-}, 1000);
+load_game();
+//start time
+update_time();
+setInterval(function () { save_game(); }, 10000);
