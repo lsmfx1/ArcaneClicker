585d1b9fa2b015e82053b02e1ad2f1066be2a97d
Baxter Campbell <lsmfx1@hotmail.com>
2014-02-28 18:46:49 -0800

    Add save import export,some cleaning, buttons

--- AClickWebsite/ArcaneClickScript.js
+++ AClickWebsite/ArcaneClickScript.js
@@ -1,6 +1,6 @@
 ﻿const increaseRate = 1.25;
-var puddleCost = 10;
-
+const puddleCostStart = 10;
+var timeSinceLastSave = 1;
 var player = {
 totalClicks: 0,
 magick: 0,
@@ -8,13 +8,42 @@ puddles: 0,
 interval_auto: null
 }
 
-function save_game() {
-    localStorage['ArcaneClicker_save'] = btoa(JSON.stringify(player));
+function reset_game() {
+    if (confirm('Are you sure you want to delete all stats?')) {
+        player.totalClicks = 0;
+        player.magick = 0;
+        player.puddles = 0;
+        player.interval_auto = null;
+        save_game();
+        update_view();
+    }
 }
 
+function save_game() {
+    localStorage['ArcaneClicker_save'] = btoa(JSON.stringify(player));
+    if (!localStorage['ArcaneClicker_save']) return alert("Save failed");
+    timeSinceLastSave = 1;
+    update_view();
+}
+
+function exportClick() {
+    if (!localStorage['ArcaneClicker_save']) return alert("No game data saved yet!");
+    prompt("Please Copy this text!", localStorage['ArcaneClicker_save']);
+}
+
+function importClick() {
+    var importValue = prompt("Please paste in provided text from export");
+    if (!importValue) return; //Remove this message
+    load_game_string(importValue);
+}
 function load_game() {
     if (!localStorage['ArcaneClicker_save']) return;
-    var save_data = JSON.parse(atob(localStorage['ArcaneClicker_save']));
+    load_game_string(localStorage['ArcaneClicker_save']);
+}
+
+function load_game_string(loadedGame)
+{
+    var save_data = JSON.parse(atob(loadedGame));
     player = save_data;
     update_view();
 }
@@ -29,15 +58,24 @@ function puddleClick(number) {
     if (player.magick >= puddleCost) {
         player.puddles++;
         player.magick -= puddleCost;
-        puddleCost = Math.floor(10 * Math.pow(increaseRate,player.puddles));
     }
     update_view();
 };
 
 function update_view() {
-    document.getElementById('puddles').innerHTML = player.puddles;
     document.getElementById("magick").innerHTML = player.magick;
+    document.title = player.magick + "¤ | Arcane Clicker";
+    document.getElementById('puddles').innerHTML = player.puddles;
+    puddleCost = Math.floor(puddleCostStart * Math.pow(increaseRate, player.puddles));
     document.getElementById("puddleCost").innerHTML = puddleCost;
+    document.getElementById("lastSave").innerHTML = timeSinceLastSave;
+}
+
+function setShortenedTime(location, value)
+{
+    //4 K 7 M 11 B 14 T 17 Qi 20 Qa 23 Sx 26 Sp
+    //should store in an array
+    var letter = "";
 }
 
 function update_time() {
@@ -47,7 +85,12 @@ function update_time() {
     }, 1000);
 }
 
-load_game();
 //start time
 update_time();
-setInterval(function () { save_game(); }, 10000);
+
+load_game();
+//auto-save
+setInterval(function () {
+    timeSinceLastSave == 10 ? save_game() : timeSinceLastSave++;
+    update_view();
+}, 1000);
