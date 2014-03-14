a707b3734711771450cb53b3e2b9acb4036689c7
Baxter Campbell <lsmfx1@hotmail.com>
2014-03-13 23:25:38 -0700

    Changing ArrayLocations

--- AClickWebsite/ArcaneClickScript.js
+++ AClickWebsite/ArcaneClickScript.js
@@ -1,11 +1,11 @@
 ﻿var SaveValuesEnum = Object.freeze({
-    totalMagickClicks: 0,
+    interval_auto: 0,//0 for loop
     magick: 1,
-    maxMagick: 2,
-    pure: 3,
-    puddles: 4,
-    hasBasicBook: 5,
-    interval_auto: 6
+    maxMagick: 2,//1-9 for basic things
+    pure: 10, //10-99 for elements
+    puddles: 100, //100-199 for specials
+    hasBasicBook: 200, //200-500 for variables
+    totalMagickClicks: 500, //500+ for stats
 });
 const increaseRate = 1.25;
 const pureCostStart = 10;
@@ -17,13 +17,13 @@ var puddleCost = puddleCostStart;
 var player = new Array();
 player_defults();
 function player_defults() {
-    player[SaveValuesEnum.totalMagickClicks] = 0;
+    player[SaveValuesEnum.interval_auto] = null;
     player[SaveValuesEnum.magick] = 0;
     player[SaveValuesEnum.maxMagick] = 50;
     player[SaveValuesEnum.pure] = 0;
     player[SaveValuesEnum.puddles] = 0;
     player[SaveValuesEnum.hasBasicBook] = 0;
-    player[SaveValuesEnum.interval_auto] = null;
+    player[SaveValuesEnum.totalMagickClicks] = 0;
 }
 
 Array.prototype.loadData = function (loadArray) {
